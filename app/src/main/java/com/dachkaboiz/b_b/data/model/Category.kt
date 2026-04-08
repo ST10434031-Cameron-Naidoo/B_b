51bca9c60a7ff44a37c16b6bbfce851f20ef8335
ST10434029-Paballo-Raphadana <163293707+ST10434029-Paballo-Raphadana@users.noreply.github.com>
2026-04-08 20:35:19 +0200

    Added category entity

--- app/src/main/java/com/dachkaboiz/b_b/data/model/Category.kt
+++ app/src/main/java/com/dachkaboiz/b_b/data/model/Category.kt
@@ -1,9 +1,26 @@
 package com.dachkaboiz.b_b.data.model
 
-import androidx.room.Entity;
-import androidx.room.PrimaryKey;
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.ForeignKey
 
-@Entity(tableName = "categories")
+@Entity(tableName = "categories",
+    foreignKeys = [
+        ForeignKey(
+            entity = User::class,
+            parentColumns = ["username"],
+            childColumns = ["accountUsername"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+    )
 data class Category(
-    val id: Int
+    val id: Int,
+    val accountUsername: String,
+    val name: String,
+    val iconName: String,
+    val description: String,
+    val minGoal: Double? = null, //optional
+    val maxGoal: Double? = null, //optional
+    val mainCategoryId: Int? = null //depends on if has a subcategory
 )
