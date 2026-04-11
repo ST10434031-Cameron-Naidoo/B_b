5267dea2998dd362695b185389ad045a07c97f58
St10439633-Jordan-Harding <163293676+St10439633-Jordan-Harding@users.noreply.github.com>
2026-04-11 23:23:57 +0200

    Created category activity and catergory xml

---
+++ app/src/main/java/com/dachkaboiz/b_b/ui/CategoryActivity.kt
@@ -0,0 +1,17 @@
+package com.dachkaboiz.b_b.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dachkaboiz.b_b.R
+
+class CategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_category)
+
+    }
+}
