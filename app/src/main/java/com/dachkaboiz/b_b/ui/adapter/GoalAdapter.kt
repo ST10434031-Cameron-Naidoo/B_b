4c7be95a02bb317d4786749125f2a53381fceb12
ST10434029-Paballo-Raphadana <163293707+ST10434029-Paballo-Raphadana@users.noreply.github.com>
2026-04-12 03:51:06 +0200

    Update goal adapter
    removed extra code

--- app/src/main/java/com/dachkaboiz/b_b/ui/adapter/GoalAdapter.kt
+++ app/src/main/java/com/dachkaboiz/b_b/ui/adapter/GoalAdapter.kt
@@ -1,4 +1,13 @@
 package com.dachkaboiz.b_b.ui.adapter
 
-class GoalAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import java.text.NumberFormat
+import java.util.Locale
+
+class   GoalAdapter {
+
 }
